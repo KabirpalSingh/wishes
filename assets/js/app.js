cfe2b8489977bcfaf5759cfc66118c11afc03dbf
Talwinder Singh <singhtalwinder790@gmail.com>
2021-01-01 21:38:15 +0530

    :sparkles: Add person name in message

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,30 @@
-/* ---- particles.js config ---- */
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (decodeURIComponent(pair[0]) == variable) {
+      return decodeURIComponent(pair[1]);
+    }
+  }
+  console.log("Query variable %s not found", variable);
+}
+
+const message = document.getElementById("message");
+
+const person = getQueryVariable("name");
+
+console.log(person);
+
+if (person) {
+  message.innerHTML = `Dear <b>${person}</b>, may your life be as bright as Sun 🌄, as 
+  beautiful as flowers 🌻, and you be showered with blessings and happiness 😋. 
+  Happy New Year 2021`;
+} else {
+  message.innerHTML = `Dear, may your life be as bright as Sun 🌄, as 
+  beautiful as flowers 🌻, and you be showered with blessings and happiness 😋. 
+  Happy New Year 2021`;
+}
 
 function removeStart() {
   const start = document.getElementById("start");
